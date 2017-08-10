75f618399a8fdf4ccd5b95bf1fe9308fc17b9966
Nick Asmussen <nick.asmussen1@gmail.com>
2017-08-10 14:38:11 -0700

    directory scaffolding and initial dependencies

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const express = require('express');
+const debug = require('debug')('parkify:server');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  debug(`listening on: ${PORT}`);
+});
