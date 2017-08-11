68823816dcce52de20399524a0b4aabbc755c95d
sharmarke <smarky292@gmail.com>
2017-08-10 21:36:09 -0700

    replaces 'username' with 'name'

--- model/user.js
+++ model/user.js
@@ -11,7 +11,7 @@ const debug = require('debug')('parkify:user');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   findHash: { type: String, unique: true }
