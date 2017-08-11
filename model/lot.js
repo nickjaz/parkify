2b95996d890aadfd4304907d137af92d1e376ea3
Nick Asmussen <nick.asmussen1@gmail.com>
2017-08-11 09:59:55 -0700

    adds spot ref to lot model

--- model/lot.js
+++ model/lot.js
@@ -8,7 +8,9 @@ const lotSchema = Schema({
   description: { type: String },
   address: { type: String, required: true, unique: true },
   userID: { type: Schema.Types.ObjectId, required: true },
-  spots: { type: Number, required: true },
+  hostID: { type: Schema.Types.ObjectId, required: true },
+  spots: [{ type: Schema.Types.ObjectId, ref: 'spot' }],
+  //not sure if we need spots taken property
   spotsTaken: { type: Number, required: true }
 });
 
--- route/lot-router.js
+++ route/lot-router.js
@@ -1,14 +1,23 @@
 'use strict';
 
- const Router = require('express').Router;
- const jsonParser = require('body-parser').json();
- const debug = require('debug')('parkify:lot-router');
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
+const debug = require('debug')('parkify:lot-router');
 
- const Lot = require('../model/lot.js');
- const bearerAuth = require('../lib/bearer-auth-middleware.js');
+const Lot = require('../model/lot.js');
+const bearerAuth = require('../lib/bearer-auth-middleware.js');
 
- const lotRouter = module.exports = Router();
+const lotRouter = module.exports = Router();
 
- lotRouter.post('/api/lot', bearerAuth, jsonParser function(request, response, next) {
-   debug('POST: /api/band')
- })
+lotRouter.post('/api/lot', bearerAuth, jsonParser, function(request, response, next) {
+  debug('POST: /api/band');
+
+  if (Object.keys(request.body).length === 0) return next(createError(400, 'Bad Request'));
+
+  request.body.userID = request.user._id;
+
+  new Lot(request.body).save()
+  .then( lot => response.json(lot))
+  .catch(next);
+});
