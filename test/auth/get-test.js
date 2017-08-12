8097aa039c7d1f539e6addd3f6acce78f14be447
sharmarke <smarky292@gmail.com>
2017-08-12 10:33:01 -0700

    tests are timing out

---
+++ test/auth/get-test.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const User = require('../../model/user.js');
+
+require('../../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  name: 'exampleuser',
+  password: '1234',
+  email: 'exampleuser@test.com'
+};
+
+describe('Auth Routes', function () {
+  describe('GET: /api/signin', function () {
+    describe('with a valid body', function () {
+      before(done => {
+        let user = new User(exampleUser);
+        user.generatePasswordHash(exampleUser.password)
+        .then(user => user.save())
+        .then(user => {
+          this.tempUser = user;
+          done();
+        });
+      });
+
+      after(done => {
+        User.remove({})
+        .then(() => done())
+        .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.get(`${url}/api/signin`)
+        .auth('exampleuser', '1234')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        });
+      });
+    });
+  });
+});
---
+++ test/auth/post-test.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const User = require('../../model/user.js');
+
+require('../../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  name: 'exampleuser',
+  password: '1234',
+  email: 'exampleuser@test.com'
+};
+
+describe('Auth Routes', function () {
+  describe('POST: /api/signup', function () {
+    describe('with a valid body', function () {
+      after(done => {
+        User.remove({})
+        .then(() => done())
+        .catch(done);
+      });
+      it('should return a token', done => {
+        request.post(`${url}/api/signup`)
+        .send(exampleUser)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.text).to.be.a('string');
+          done();
+        });
+      });
+    });
+  });
+});
