ba8e5a4faa1d5d459fb0b89d37a2f6e54ed03ddd
sharmarke <smarky292@gmail.com>
2017-08-13 15:59:17 -0700

    writes get and post test

---
+++ test/auth/post-test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const User = require('../../model/user.js');
+
+require('../../server.js');
+
+const url = `http://localhost:${process.env.PORT}`;
+
+const exampleUser = {
+  name: 'exampleuser',
+  password: '1234',
+  email: 'exampleuser@test.com'
+}
+
+
+describe('Auth Routes', function () {
+  describe('POST: /api/signup', function () {
+    describe('with a valid body', function () {
+      after(done => {
+        User.remove({})
+          .then(() => done())
+          .catch(done);
+      });
+
+      it('should return a token', done => {
+        request.post(`${url}/api/signup`)
+          .send(exampleUser)
+          .end((err, result) => {
+            if (err) return done(err);
+            expect(resonse.status).to.equal(200);
+            expect(resonse.text).to.be.a('string');
+            done();
+          });
+      });
+    });
+  });
+});
