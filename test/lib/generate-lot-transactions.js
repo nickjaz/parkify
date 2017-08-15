aefac2394b675b6d1850b7985d8889bd7a46e029
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-08-15 14:45:50 -0700

    Adds predictor test.

--- test/lib/generate-lot-transactions.js
+++ test/lib/generate-lot-transactions.js
@@ -1,47 +1,61 @@
 'use strict';
 
+const request = require('superagent');
 const generateUser = require('./generate-user.js');
 const generateLot = require('./generate-lot.js');
 const generateSpot = require('./generate-spot.js');
-const generateTransaction = require('./generate-user.js');
-const NodeGeocoder = require('node-geocoder');
+const generateTransaction = require('./generate-transaction.js');
+// const NodeGeocoder = require('node-geocoder');
 
-let generateLotTransactions = function(sampleCount, startDate, endDate, address, distanceRadius=500) {
-  return new Promise((resolve, reject) => {
-
-    const placeQuery = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.6182,122.3519&radius=${distanceRadius}&key=${process.env.GOOGLE_API_KEY}`;
-    
+let generateLotTransactions = function(sampleCount, startDate, endDate, address, distanceRadius = 500) {
+  return new Promise(function(resolve, reject) {
+    let context = {};
 
     generateUser()
     .then(data => {
-      this.host = data.user;
-      this.hostToken = data.token;
+      context.host = data.user;
+      context.hostToken = data.token;
     })
     .then(() => generateUser())
     .then(data => {
-      this.guest = data.user;
-      this.guestToken = data.token;
+      context.guest = data.user;
+      context.guestToken = data.token;
     })
-    .then(() => generateLot(this.host._id))
+    .then(() => generateLot(context.host._id))
     .then(lot => {
-      this.lot = lot;
+      context.lot = lot;
     })
-    .then(() => generateSpot(this.lot._id))
+    .then(() => generateSpot(context.lot._id))
     .then(spot => {
-      this.spot = spot;
+      context.spot = spot;
     })
-    .then(() => Promise.all(generateTransactionData(sampleCount, startDate, endDate, address, distanceRadius)))    
-    .then(transactions => {
-      resolve(transactions);
-    })
+    .then(() => getNearbyPlaces(address, distanceRadius))
+    // .then(() => Promise.all(generateTransactionData(sampleCount, startDate, endDate, address, distanceRadius)))    
+    // .then(transactions => {
+    //   resolve(transactions);
+    // })
     .catch(error => reject(error));
   });
 };
 
-function generateTransactionData(sampleCount, startDate, endDate, address, distanceRadius) {
-  for (let transactionNumber = 0; transactionNumber < sampleCount; transactionNumber++) {
+// function generateTransactionData(sampleCount, startDate, endDate, address, distanceRadius) {
+  
+//   const transactions = [];
+
+//   for (let transactionNumber = 0; transactionNumber < sampleCount; transactionNumber++) {
     
-  }
+//   }
+
+// }
+
+function getNearbyPlaces(address, distanceRadius) {
+  request.get('https://maps.googleapis.com/maps/api/place/textsearch/json')
+  .query(`query=${encodeURIComponent(address)}`)
+  .query(`radius=${distanceRadius}`)
+  .query(`key=${process.env.GOOGLE_API_KEY}`)
+  .end(function(error, response) {
+    console.log(response);
+  });
 
 }
 
---
+++ test/lot-price-predictor-test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const User = require('../model/user.js');
+const Lot = require('../model/lot.js');
+const Spot = require('../model/spot.js');
+const Transaction = require('../model/transaction.js');
+const generateLotTransactions = require('./lib/generate-lot-transactions.js');
+
+require('../server.js');
+
+describe('Lot Price Predictor', function() {
+  before(done => {
+    generateLotTransactions(100, Date.now(), Date.now(), '2901 3rd Ave #300, Seattle, WA 98121', 500)
+    .then(done);
+  });
+
+  after(done => {
+    Promise.all([
+      User.remove({}),
+      Lot.remove({}),
+      Spot.remove({}),
+      Transaction.remove({})
+    ])
+    .then(() => done());
+  });
+
+  describe('Valid Request', () => {
+    it('should return places.', done => {
+      done();
+    });
+  });
+});
