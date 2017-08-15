63b9a07cfeb38a3eb0b988420f57ba0fb6d2f378
Nick Asmussen <nick.asmussen1@gmail.com>
2017-08-15 11:53:47 -0700

    fixes timeslot post test

--- test/timeslot/post-test.js
+++ test/timeslot/post-test.js
@@ -4,9 +4,9 @@ const expect = require('chai').expect;
 const request = require('superagent');
 const Promise = require('bluebird');
 
-const Spot = require('../../model/spot.js');
-const Lot = require('../../model/lot.js');
 const User = require('../../model/user.js');
+const Lot = require('../../model/lot.js');
+const Spot = require('../../model/spot.js');
 const Timeslot = require('../../model/timeslot.js');
 
 require('../../server.js');
@@ -20,14 +20,14 @@ const exampleUser = {
 };
 
 const exampleLot = {
-  name: 'example lot',
+  name: 'example lot name',
   description: 'example lot description',
-  address: 'example address',
+  address: 'example address'
 };
 
 const exampleSpot = {
-  name: 'example spot',
-  description: 'example spot description',
+  name: 'example spot name',
+  description: 'example spot description'
 };
 
 const exampleTimeslot = {
@@ -36,7 +36,8 @@ const exampleTimeslot = {
 };
 
 describe('Timeslot Post Route', function() {
-  describe('POST: /api/lot/:lotID/spot', function() {
+
+  describe('POST: /api/lot/:lotID/spot/:spotID/timeslot/:id', function() {
     before( done => {
       new User(exampleUser)
       .generatePasswordHash(exampleUser.password)
@@ -49,72 +50,79 @@ describe('Timeslot Post Route', function() {
       })
       .then( token => {
         this.tempToken = token;
+        done();
       })
-      .then( () => {
-        exampleLot.userID = this.tempUser._id.toString();
-        new Lot(exampleLot).save()
-        .then( lot => {
-          this.tempLot = lot;
+      .catch(done);
+    });
+
+    before( done => {
+      exampleLot.userID = this.tempUser._id.toString();
+      new Lot(exampleLot).save()
+      .then( lot => {
+        this.tempLot = lot;
+        done();
+      })
+      .catch(done);
+    });
+
+    before( done => {
+      exampleSpot.lotID = this.tempLot._id.toString();
+      new Spot(exampleSpot).save()
+      .then( spot => {
+        this.tempSpot = spot;
+        done();
+      })
+      .catch(done);
+    });
+
+    after( done => {
+      Promise.all([
+        User.remove({}),
+        Lot.remove({}),
+        Spot.remove({}),
+        Timeslot.remove({})
+      ])
+      .then( () => done())
+      .catch(done);
+    });
+
+    describe('valid body', () => {
+      it('should return 200 and correct property values', done => {
+        request.post(`${url}/api/lot/${this.tempLot._id}/spot/${this.tempSpot._id}/timeslot`)
+        .send(exampleTimeslot)
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
         })
-        .then( () => {
-          exampleSpot.lotID = this.tempLot._id.toString();
-          new Spot(exampleSpot).save()
-          .then( spot => {
-            this.tempSpot = spot;
-            done();
-          })
-          .catch(done);
+        .end((error, response) => {
+          if(error) return done(error);
+          expect(response.status).to.equal(200);
+          expect(Date.parse(response.body.startTime)).to.equal(exampleTimeslot.startTime);
+          expect(Date.parse(response.body.endTime)).to.equal(exampleTimeslot.endTime);
+          done();
         });
+      });
+    });
 
-        after( done => {
-          Promise.all([
-            User.remove({}),
-            Lot.remove({}),
-            Spot.remove({}),
-            Timeslot.remove({})
-          ])
-          .then( () => done())
-          .catch(done);
+    describe('unauthorized request', () => {
+      it('should return 401 status code', done => {
+        request.post(`${url}/api/lot/${this.tempLot._id}/spot/${this.tempSpot._id}/timeslot`)
+        .end((error, response) => {
+          expect(response.status).to.equal(401);
+          done();
         });
+      });
+    });
 
-        describe('valid request', () => {
-          it('should return 201 status and expected property values', done => {
-            request.post(`${url}/api/lot/${this.tempLot._id}/spot/${this.tempSpot._id}/timeslot`)
-            .send(exampleTimeslot)
-            .set({
-              Authorization: `Bearer ${this.tempToken}`
-            })
-            .end((error, response) => {
-              if (error) return done(error);
-              expect(response.status).to.equal(201);
-              expect(response.body.startTime).to.equal(exampleLot.startTime);
-              expect(response.body.endTime).to.equal(exampleLot.endTime);
-              expect(response.body.spotID).to.equal(this.tempSpot._id.toString());
-              done();
-            });
-          });
-        });
-
-        describe('unauthorized request', () => {
-          it('should return 401 status code', done => {
-            request.post(`${url}/api/lot/${this.tempLot._id}/spot/${this.tempSpot._id}/timeslot`)
-            .send(exampleLot)
-            .end((error, response) => {
-              expect(response.status).to.equal(401);
-              done();
-            });
-          });
-        });
-
-        describe('invalid data', () => {
-          it('should return 400 status code', done => {
-            request.post(`${url}/api/lot/${this.tempLot._id}/spot/${this.tempSpot._id}/timeslot`)
-            .send({ startTime: 'fake time' })
-            .end((error, response) => {
-              expect(response.status).to.equal(400);
-              done();
-            });
-          });
+    describe('nonexistent id', () => {
+      it('should reutrn 400', done => {
+        request.post(`${url}/api/lot/${this.tempLot._id}/spot/${this.tempSpot._id}/timeslot`)
+        .send({ name: 'fake timeslot' })
+        .set({
+          Authorization: `Bearer ${this.tempToken}`
+        })
+        .end((error, response) => {
+          expect(response.status).to.equal(400);
+          done();
         });
       });
     });
